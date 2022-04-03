459262a913c5f754596e29d30b95234466e1b648
Catherine Choi <catherinemargaretchoi@gmail.com>
2022-04-03 19:39:01 -0400

    Add user auth routes, models, and validations

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://catherinechoi:jyZh2amXmjKcJyB0@cluster0.lb1n2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  secretOrKey: 'VBVIineH0KahyX3Jo98G7L017zTo4pFT'
+}
