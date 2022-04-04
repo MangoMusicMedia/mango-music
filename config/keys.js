47abe7a6005c866eef45239a02bd71f16f1aa3a1
kingbloopy <kingbloopy@gmail.com>
2022-04-03 20:26:38 -0700

    key

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://catherinechoi:jyZh2amXmjKcJyB0@cluster0.lb1n2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  secretOrKey: 'VBVIineH0KahyX3Jo98G7L017zTo4pFT'
+  mongoURI: 'mongodb+srv://TeamMango:LaapdnPayOeDVmbg@mango.qaglh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  secretOrKey: 'PL349iHlfr%njnnn6ttl1f699ineDGk789jsdfnPL'
 }
