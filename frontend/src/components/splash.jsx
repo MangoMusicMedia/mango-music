eede2300e50e9a8c05e921746efc189825d14182
kingbloopy <kingbloopy@gmail.com>
2022-04-04 14:07:17 -0700

    add join button

--- frontend/src/components/splash.jsx
+++ frontend/src/components/splash.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import BoomBox from '../images/boom-box.png'
+import boomBox from '../images/boom-box.png'
+import rainbow from '../images/rainbow.png';
 
 const Splash = props => {
   return (
     <div className='splash'>
       <div className='splash__inner'>
-        <img src={BoomBox}/>
+        <div className='splash__boom-wrapper'>
+          <img className='splash__boom-wrapper__boom-box' src={boomBox}/>
+        </div>
+        <div className='splash__rainbow-wrapper'>
+          <div className='splash__rainbow-wrapper__inner'>
+          <h1>Share the music you love.</h1>
+            <button className='btn draw-border'>Join now</button>
+            <img className='splash__rainbow-wrapper__rainbow' src={rainbow}/>
+          </div>
+        </div>
       </div>
     </div>
   );
