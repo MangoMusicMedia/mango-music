5c478f953b65184e6484348c7c25a96965c32752
Catherine Choi <59376544+catherine-m-choi@users.noreply.github.com>
2022-04-04 13:40:51 -0400

    Delete keys.js

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://TeamMango:Q8GBxf0NoFHLQlaT@mango.qaglh.mongodb.net/mango-music?retryWrites=true&w=majority',
-  secretOrKey: 'JNSFksnefjwebi8462hsdnskdjfballjhbhyuewue99823b4uj',
-  spotify_client_id: '7ec8a6c9e2d345a1beb572eefa1f90f5',
-  spotify_client_secret: 'e2989611564b40fbbefa71289a49adbd'
-}
