4cae7638e01c1351c66d6f8f6f97ab94ec9253eb
kingbloopy <kingbloopy@gmail.com>
2022-04-03 20:47:08 -0700

    removed keys

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://TeamMango:LaapdnPayOeDVmbg@mango.qaglh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  secretOrKey: 'PL349iHlfr%njnnn6ttl1f699ineDGk789jsdfnPL'
-}
