23881ad260c8ead46595ac034fb59ef77760abf2
shhmabbey <abhernndz@gmail.com>
2022-04-05 15:04:20 -0700

    Add profile photo

---
+++ frontend/src/components/profile/profile.jsx
@@ -0,0 +1,19 @@
+import Header from "../splash/header";
+import ProfileHeader from "./profile_header";
+
+const Profile = props => {
+
+  return (
+    <div className="profile-wrapper">
+      <ProfileHeader />
+    </div>
+    // personal posts feed
+    // button that opens sidebar to edit profile?
+  )
+
+
+
+  // scroll to top button in profile
+}
+
+export default Profile
