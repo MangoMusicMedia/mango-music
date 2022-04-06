df1b374127b215f1a762dd35689538a68f444f9c
shhmabbey <abhernndz@gmail.com>
2022-04-05 22:11:33 -0700

    Added sign in sign out button toggle

--- frontend/src/components/splash/header.jsx
+++ frontend/src/components/splash/header.jsx
@@ -1,21 +1,37 @@
 import React from "react";
 import logo from '../../images/logo.png';
 import { openModal } from "../../actions/modal_actions";
+import { logout } from "../../actions/session_actions"
 import { connect } from "react-redux";
 
-const Header = ({ openModal }) => {
+const Header = (props) => {
+  const { openModal, logout } = props;
+
+  const getSessionLink = (loggedIn = null) => {
+    if (loggedIn) {
+      return <button onClick={() => logout()} className="header__sign-in">Sign Out</button>
+    } else {
+      return <button onClick={() => openModal('login')} className="header__sign-in">Sign in</button>
+    }
+  }
+
   return (
     <header className="header">
       <div className="header__inner">
-        <img className="header__logo" src={logo}/>
-        <button onClick={() => openModal('login')} className="header__sign-in">Sign in</button>
+        <img className="header__logo" src={logo} alt="logo"/>
+        {getSessionLink(props.loggedIn) }
       </div>
     </header>
   );
 }
 
+const mapStateToProps = state => ({
+  loggedIn: state.session.isAuthenticated
+});
+
 const mapDispatchToProps = dispatch => ({
-  openModal: modal => dispatch(openModal(modal))
+  openModal: modal => dispatch(openModal(modal)),
+  logout: () => dispatch(logout())
 })
 
-export default connect(null, mapDispatchToProps)(Header);
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
