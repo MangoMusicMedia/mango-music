c586f0031671f3bb9ceeee6fddcb148e132a613a
Catherine Choi <catherinemargaretchoi@gmail.com>
2022-04-06 11:51:59 -0400

    Prepare keys for deployment

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,6 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.SECRET_OR_KEY
+  secretOrKey: process.env.SECRET_OR_KEY,
+  spotify_client_id: process.env.SPOTIFY_CLIENT_ID,
+  spotify_client_secret: process.env.SPOTIFY_CLIENT_SECRET
 }
