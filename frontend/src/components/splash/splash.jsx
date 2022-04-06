f09215d8cdcca7c56985dd47a4e4b8271b0196ec
kingbloopy <kingbloopy@gmail.com>
2022-04-05 22:49:16 -0700

    -mreconfigured splash

--- frontend/src/components/splash/splash.jsx
+++ frontend/src/components/splash/splash.jsx
@@ -18,26 +18,35 @@ const Splash = ({ openModal }) => {
   return (
     <div className='splash'>
       <div className='splash__inner'>
-        <div className='splash__boom-wrapper'>
-          <img className='splash__boom-wrapper__boom-box' src={boomBox}/>
-        </div>
-        <div className='splash__rainbow-wrapper'>
-          <div className='splash__rainbow-wrapper__inner'>
-            <ReactRotatingText className="moving-words" typingInterval={120} deletingInterval={100} items={headerContent} />
-            <button onClick={handleOpenModal} className='btn draw-border'>Join now</button>
-            <img className='splash__rainbow-wrapper__rainbow' src={rainbow}/>
+        <div className='splash__left'>
+          <div className='splash__left__rainbow-wrapper'>
+            <div className='splash__left__rainbow-wrapper__inner'>
+              <ReactRotatingText
+              className="moving-words"
+              typingInterval={120}
+              deletingInterval={100}
+              items={headerContent} 
+              />
+              <div className='splash__button-wrapper'>
+                <button onClick={handleOpenModal} className='btn draw-border'>Join now</button>
+                <img className='splash__left__rainbow-img' src={rainbow}/>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-
-      <div className='splash__wave-wrapper'>
-        <MusicWave/>
-      </div>
-
-      <div className='splash__words'>
-        <div className='words-wrapper'>
-          <h1>Why Mango Music?</h1>
-          <p>Join a collective community of devote music lovers with Mango Music. As a Mango user, you can share your favorite songs or artists with other likeminded people in order to broaded your musical horizon and explore new musical tastes and sounds. Join now for free to explore all that Mango Music has to offer.</p>
+        <div className='splash__right'>
+          <div className='splash__right__boom-wrapper'>
+            <img className='splash__right__boom-box' src={boomBox} />
+          </div>
+          <div className='splash__right__wave-wrapper'>
+            <MusicWave/>
+          </div>
+          <div className='splash__right__words'>
+            <div className='splash__right__words-wrapper'>
+              <h1>Why Mango Music?</h1>
+              <p>Join a collective community of devote music lovers with Mango Music. As a Mango user, you can share your favorite songs or artists with other likeminded people in order to broaded your musical horizon and explore new musical tastes and sounds. Join now for free to explore all that Mango Music has to offer.</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
