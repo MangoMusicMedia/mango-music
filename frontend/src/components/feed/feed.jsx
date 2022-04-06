c626da03b49ea349c09da33de4064c05e325cd53
kingbloopy <kingbloopy@gmail.com>
2022-04-06 16:41:41 -0700

    tester grid

--- frontend/src/components/feed/feed.jsx
+++ frontend/src/components/feed/feed.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import testPic from '../../images/test.jpg'
+import FeedPost from './feed_post';
 
 const Feed = props => {
+  const tester = {img: testPic, description: 'This is a tester description for the most awesome song that I have ever heard! I love this band!!'}
+  const posts = [];
+  while (posts.length < 50){
+    posts.push(tester);
+  }
+
   return (
-    <div>
-      FEED ME
+    <div className='feed'>
+      <ul className='feed__container'>
+        {posts.map(post => (
+          <FeedPost img={post.img} text={post.description}/>
+        ))}
+      </ul>
     </div>
   );
 }
