495f8b7a210562be49dc6da82ecfc8840c038ca7
shhmabbey <abhernndz@gmail.com>
2022-04-05 23:09:00 -0700

    Heroku config keys

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+// HEROKU
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+// 
+
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
