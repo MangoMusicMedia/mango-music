bd7a21e94ccab2128a8253e209e7ecc17e5a6bce
Catherine Choi <catherinemargaretchoi@gmail.com>
2022-04-07 00:50:00 -0400

    Update seeds with custom songIds

--- seeds.js
+++ seeds.js
@@ -26,70 +26,62 @@ Post.remove({}, function(err) {
   console.log('User collection removed') 
 });
 
-let demoPassword = "password123";
-bcrypt.genSalt(10, (err, salt) => {
-  bcrypt.hash(demoPassword, salt, (err, hash) => {
-    demoPassword = hash;
-  })
-})
+const fakeUserData = [];
+for (let idx = 0; idx < 100; idx++) {
+  const firstName = faker.name.firstName();
+  const lastName = faker.name.lastName();
+  let newUser = {
+    'username': faker.internet.userName(firstName, lastName),
+    'email': faker.internet.email(firstName, lastName),
+    'password': 'password'
+  }
+  fakeUserData.push(new User(newUser))
+}
 
-let genericPassword = "password";
-bcrypt.genSalt(10, (err, salt) => {
-  bcrypt.hash(genericPassword, salt, (err, hash) => {
-    genericPassword = hash;
-  })
-})
-
-const demoUser = {
+let newDemoUser = {
   'username': 'demouser',
   'email': 'demouser@email.com',
-  'password': 'password123'
+  'password': '$2a$10$aOMJ3eVpcopmn71gewSVte3h15VlB3nKk1fgEvHfjmjBe2dpZnnVq'
 }
 
-const newCatUser = {
+let newCatUser = {
   'username': 'catchoi',
   'email': 'catherine-test@gmail.com',
-  'password': genericPassword
+  'password': '$2a$10$aOMJ3eVpcopmn71gewSVte3h15VlB3nKk1fgEvHfjmjBe2dpZnnVq'
 }
 
-const newMaggieUser = {
+let newMaggieUser = {
   'username': 'kingbloopy',
   'email': 'maggie-test@gmail.com',
-  'password': genericPassword
+  'password': '$2a$10$aOMJ3eVpcopmn71gewSVte3h15VlB3nKk1fgEvHfjmjBe2dpZnnVq'
 }
 
-const newAbbeyUser = {
+let newAbbeyUser = {
   'username': 'Shhmabbey',
   'email': 'abbey-test@gmail.com',
-  'password': genericPassword
+  'password': '$2a$10$aOMJ3eVpcopmn71gewSVte3h15VlB3nKk1fgEvHfjmjBe2dpZnnVq'
 }
 
-const newJohnnyUser = {
+let newJohnnyUser = {
   'username': 'johnnyhoang510',
   'email': 'johnny-test@gmail.com',
-  'password': genericPassword
+  'password': '$2a$10$aOMJ3eVpcopmn71gewSVte3h15VlB3nKk1fgEvHfjmjBe2dpZnnVq'
 }
 
-const usersData = [   
-  new User(demoUser),
+let usersData = [
+  new User(newDemoUser),
   new User(newCatUser),
   new User(newMaggieUser),
   new User(newAbbeyUser),
   new User(newJohnnyUser),
+  ...fakeUserData
 ]
 
-for (let idx = 0; idx < 100; idx++) {
-  const firstName = faker.name.firstName();
-  const lastName = faker.name.lastName();
-
-  let newUser = new User({
-    'username': faker.internet.userName(firstName, lastName),
-    'email': faker.internet.email(firstName, lastName),
-    'password': genericPassword
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash("password123", salt, (err, hash) => {
+    console.log(hash);
   })
-
-  usersData.push(newUser)
-}
+})
 
 usersData.map(async (user, index) => {
   await user.save((err, result) => {
@@ -99,45 +91,75 @@ usersData.map(async (user, index) => {
   });
 })
 
+const songIds = [
+  "7qCZgvV98j6RjUULW1s1it", 
+  "3UYFbRnkJlvm4POhrtsEL7", 
+  "43NI5sAcvDLG7QQAmUc7UU", 
+  "3i2pAVmEzBgJIHtIMkNQBJ", 
+  "4c7k0jGOFR1OraHvE3BszV", 
+  "04oQ19JkxyzzLglnqYsrlB", 
+  "3kzOYFM5ZTj45uLhpSoINZ", 
+  "29rx2LiCOpZ6PYFc8l4M3z", 
+  "0ZfP7K8NoyJRjEfWWk8Mlv", 
+  "1595LW73XBxkRk2ciQOHfr", 
+  "5KUNwkaNf8l5A9sXZhiCgI", 
+  "2P5yIMu2DNeMXTyOANKS6k", 
+  "5AJrhrwz4oSZX2PwwV4qrN", 
+  "2zQl59dZMzwhrmeSBEgiXY", 
+  "2Nh2cMryoXl7BrZoIeN2Pr", 
+  "5WNYg3usc6H8N3MBEp4zVk", 
+  "1DrlLvlYd1FIjNavRm6NdX", 
+  "1p6AQlMFpnH4hmhrSfoQ3k", 
+  "5Go7a9wnApTQm2nYqKBdm0", 
+  "0jWgAnTrNZmOGmqgvHhZEm", 
+  "1Cv1YLb4q0RzL6pybtaMLo", 
+  "4DAaQ5InUO23d8yNRbB0Yj", 
+  "5bBUDJUfGcG7eFy3Bf4fXv", 
+  "2H7PHVdQ3mXqEHXcvclTB0", 
+  "6nozDLxeL0TE4MS9GqYU1v", 
+  "0OMmiWMwsCNtpQ5aP6fdp9", 
+  "4xlpJ99yL9xYQtzG6c3hwk", 
+  "2wQVmS0j4xcSbEK8CLEgwz", 
+  "1XQhZctQWzkznclbmbE7FQ", 
+  "362zcsyXMLbL7PNLhOovvm", 
+  "1jJci4qxiYcOHhQR247rEU", 
+  "00WvmRXTkPBZNhhRK3xfdy"
+]
 
-axios.get('http://localhost:5000/api/spotify/new-releases', {country: 'US'})
+
+let postData = []
+songIds.forEach(async (songId, idx) => {
+  let newPost;
+  await axios.get('http://localhost:5000/api/spotify/track', { params: { id: songId } })
   .then(res => {
-    console.log("successful fetch!")
-    return res.data.albums.items
-  }, err => {
-    console.log("fail to fetch!")
-    console.log(err)
-  })
-  .then( (newReleases) => {
-    const postData = []
-    newReleases.forEach(item => {
-      const randUser = usersData[Math.floor(Math.random() * usersData.length)];
-      if (item.album_type === "single") {
-        const newPost = new Post({
-          title: faker.lorem.sentence(),
-          description: faker.lorem.paragraph(),
-          author: randUser._id,
-          likes: [],
-          comments: [],
-          trackName: item.name,
-          trackId: item.id,
-          albumCoverURL: item.images[0].url,
-          albumName: item.name,
-          releaseDate: item.release_date,
-        })
-        postData.push(newPost)
-      }
+    console.log("Fetch new releases!")
+    const song = res.data
+    // console.log(song)
+    const randUser = usersData[Math.floor(Math.random() * usersData.length)];
+    newPost = new Post({
+      title: faker.lorem.sentence(),
+      description: faker.lorem.paragraph(),
+      author: randUser._id,
+      likes: [],
+      comments: [],
+      trackName: song.name,
+      trackId: song.id,
+      albumCoverURL: song.album.images[0].url,
+      albumName: song.album.name,
+      releaseDate: song.album.release_date,
     })
-
-    postData.map(async (post, index) => {
-      await post.save((err, result) => {
-        if (index === postData.length - 1) {
-          console.log("Posts seeded!");
-          mongoose.disconnect();
-        }
-      });
+    // postData.push(newPost)
+    return newPost
+  }, err => console.log(err))
+  .then( async (post) => {
+    await post.save((err, result) => {
+      if (idx === songIds.length - 1) {
+        console.log("Posts seeded!");
+        mongoose.disconnect();
+      } 
     })
-
-    return postData
   })
+
+})
+
   
