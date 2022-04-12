831a1a3adf5dd1f4a7e0fead38f228c1dd7bbaac
Catherine Choi <catherinemargaretchoi@gmail.com>
2022-04-12 18:43:26 -0400

    Another fix to keys_prod

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -3,7 +3,7 @@ module.exports = {
   secretOrKey: process.env.SECRET_OR_KEY,
   spotify_client_id: process.env.SPOTIFY_CLIENT_ID,
   spotify_client_secret: process.env.SPOTIFY_CLIENT_SECRET,
-  genius_client_id: process.env.GENIUS_AUTHORIZATION_TOKEN,
-  genius_client_secret: process.env.GENIUS_CLIENT_ID,
-  genius_authorization_token: process.env.GENIUS_CLIENT_SECRET
+  genius_client_id: process.env.GENIUS_CLIENT_ID,
+  genius_client_secret: process.env.GENIUS_CLIENT_SECRET,
+  genius_authorization_token: process.env.GENIUS_AUTHORIZATION_TOKEN
 }
