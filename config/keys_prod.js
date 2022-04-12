112d80ffbefa9d78463e18b2dcc0cea2dae44fd8
Catherine Choi <catherinemargaretchoi@gmail.com>
2022-04-12 17:47:19 -0400

    Add genius keys to keys_prod

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -2,5 +2,8 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   secretOrKey: process.env.SECRET_OR_KEY,
   spotify_client_id: process.env.SPOTIFY_CLIENT_ID,
-  spotify_client_secret: process.env.SPOTIFY_CLIENT_SECRET
+  spotify_client_secret: process.env.SPOTIFY_CLIENT_SECRET,
+  genius_client_id: process.env.GENIUS_AUTHORIZATION_TOKEN,
+  genius_client_secret: process.env.GENIUS_CLIENT_ID,
+  genius_authorization_token: process.env.GENIUS_CLIENT_SECRET
 }
